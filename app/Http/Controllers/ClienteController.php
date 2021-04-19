baebb1671dd09e87335139b598bdbf6a96606c16
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-04-19 10:19:03 -0300

    alteração nos arquivos php controle do projeto.

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -7,7 +7,7 @@ use App\Models\Cliente;
 
 class ClienteController extends Controller
 {
-    //
+    // add_customer_form
     public function add_cliente_form()
     {
         if (\View::exists('cliente.criar')) {
@@ -17,6 +17,7 @@ class ClienteController extends Controller
         }
     }
 
+    // submit_customer_data
     public function enviar_cliente_dado(Request $request)
     {
         $rules = [
@@ -25,25 +26,33 @@ class ClienteController extends Controller
         ];
 
         $mensagemErros = [
-            'required' => 'Por Favor seu :atribua primeiro.'
+            'required' => 'Enter your :attribute first.'
         ];
+
+        $this->validate($request, $rules, $mensagemErros);
 
         Cliente::create([
             'nome' => $request->nome,
             'slug' => \Str::slug($request->nome),
             'email' => strtolower($request->email)
         ]);
-        $this->mensagem('mensagem', 'Clinte cadastrado com sucesso!.');
+        $this->mensagem('mensagem', 'Cliente cadastrado com sucesso!.');
 
         return redirect()->back();
     }
+    // fetch_all_customer
     public function buscar_todos_clientes()
     {
         $clientes =Cliente::toBase()->get();
 
+        return view('cliente.index', compact('clientes'));
+    }
+    //edit_customer_form
+    public function editar_cliente_form(Cliente $cliente)
+    {
         return view('cliente.editar', compact('cliente'));
     }
-
+    // edit_customer_form_submit
     public function editar_cliente_form_enviar(Request $request, Cliente $cliente)
     {
         $rules = [
@@ -51,7 +60,7 @@ class ClienteController extends Controller
             'email' => 'required|email|unique:clientes'
         ];
         $mensagemErros = [
-            'required' => 'Entre seu :atribua primeiro.'
+            'required' => 'Enter your :attribute first.'
         ];
 
         $this->validate($request, $rules, $mensagemErros);
@@ -65,13 +74,16 @@ class ClienteController extends Controller
 
         return redirect()->back();
     }
+    // view_single_customer
     public function mostrar_unico_cliente(Cliente $cliente)
     {
         return view('cliente.mostrar', compact('cliente'));
     }
+
+    // delete_customer
     public function deletar_cliente(Cliente $cliente)
     {
-        $cliente->deletar();
+        $cliente->delete();
         $this->mensagem('mensagem', 'Cliente deletado com sucesso!.');
 
         return redirect()->back();
--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,10 @@ Route::get('/', function () {
 
 Route::get('/cliente', 'ClienteController@add_cliente_form')->name('cliente.add');
 Route::post('/cliente', 'ClienteController@enviar_cliente_dado')->name('cliente.salvar');
-Route::get('/cliente/lista', 'ClienteController@buscar_todos_clientes')->name('cliente.listar');
+Route::get('/cliente/listar', 'ClienteController@buscar_todos_clientes')->name('cliente.listar');
 Route::get('/cliente/editar{cliente}', 'ClienteController@editar_cliente_form')->name('cliente.editar');
-Route::patch('/cliente/editar{cliente}', 'ClienteController@editar_cliente_form_enviar')->name('cliente.atualizar');
+Route::patch('/cliente/editar{cliente}', 'ClienteController@editar_cliente_form_enviar')->name('cliente.update');
 Route::get('/cliente/{cliente}', 'ClienteController@mostrar_unico_cliente')->name('cliente.mostrar');
-Route::delete('/cliente/{cliente}', 'ClienteController@deletar_cliente')->name('cliente.deletar');
+Route::delete('/cliente/{cliente}', 'ClienteController@deletar_cliente')->name('cliente.delete');
+
+
